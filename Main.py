e122167cc255544ce7485491f719c979cf620b34
gregory goren <grggoren@gmail.com>
2017-01-23 12:21:54 +0200

    basic model running

--- Main.py
+++ Main.py
@@ -6,12 +6,14 @@ import numpy as np
 from scipy.sparse import csr_matrix
 import time
 if __name__ == '__main__':
-    train_dict, golden_standard = Pre.get_file_as_dict("test") #some
+    train_dict, golden_standard = Pre.get_file_as_dict("train") #some
     f = fmaker.Feature_maker(train_dict,golden_standard)
     init_start = time.time()
     f.init_all_features_indexes()
     print("initialization of features took ",time.time()-init_start)
-    p = P.Perceptron(f,20)
-    p.run()
-    test_set = Pre.get_file_as_dict("test")[0]
-
+    iterations =[20,50,80,100]
+    for iteration in iterations:
+        p = P.Perceptron(f,iteration)
+        p.run()
+        test_set,golden = Pre.get_file_as_dict("test")
+        p.inference(test_set,False,golden)
