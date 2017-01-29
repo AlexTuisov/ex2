764a4aea908eb7aedec108cdcd7854c74d3bbee5
gregory goren <grggoren@gmail.com>
2017-01-29 16:48:17 +0200

    competition results adding

--- Main.py
+++ Main.py
@@ -9,21 +9,20 @@ def accuracy_measure(num_of_iterations, feature_maker):
     p = P.Perceptron(feature_maker, num_of_iterations)
     p.run()
     test_set, golden = Pre.get_file_as_dict("test")
-    return (p.inference(test_set, False, golden))
+    return (p.inference(test_set, True, golden))
 
 if __name__ == '__main__':
-    with Pool(4) as my_pool:
-        train_dict, golden_standard = Pre.get_file_as_dict("train") #some
-        f = fmaker.Feature_maker(train_dict,golden_standard,False)
-        init_start = time.time()
-        f.init_all_features_indexes()
-        print("initialization of features took ", time.time()-init_start)
-        iterations = [20,50,80,100]
-        makers = [f]*4
-        input_pool = zip(iterations, makers)
-        list_of_accuracies = my_pool.starmap(accuracy_measure, input_pool)
-        print(list_of_accuracies)
-
-
+    train_dict, golden_standard = Pre.get_file_as_dict("train") #some
+    f = fmaker.Feature_maker(train_dict,golden_standard,True)
+    init_start = time.time()
+    f.init_all_features_indexes()
+    print("initialization of features took ", time.time()-init_start)
+    #list_of_accuracies = my_pool.starmap(accuracy_measure, input_pool)
+    #print(list_of_accuracies)
+    p = P.Perceptron(f,1)
+    p.run()
+    test_set, golden = Pre.get_file_as_dict("competition")
+    results_of_competition = p.inference(test_set, True, golden)
+    Pre.print_the_results(results_of_competition)
 
 
